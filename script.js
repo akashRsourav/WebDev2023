013bbb7a5ec6277df84c31a1458675a085392869
akashRsourav <akashsourav800@gmail.com>
2023-02-23 00:15:48 -0500

    Color Change function added, Website done!

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function changeColor()
+{
+    let color = document.body.style.color;
+    if (color === 'black')
+    {
+        document.body.style.color = 'blue';
+    }
+    else
+    {
+        document.body.style.color = 'black';
+    }
+}
